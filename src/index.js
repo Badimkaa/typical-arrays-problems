86fc7941e52bc294f1bfde54ffd512469e910737
Badimkaa <glopu19@gmail.com>
2021-03-14 18:11:54 +0300

    feat: add solution for task Typical array problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
-  return 0;
+    if ( array === undefined || array.length === 0 ) {
+        return 0
+    }
+    let res = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < res)
+        {
+            res = array[i]
+        }
+    }
+  return res;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if ( array === undefined || array.length === 0 ) {
+        return 0
+    }
+    let res = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] > res)
+        {
+            res = array[i]
+        }
+    }
+  return res;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if ( array === undefined || array.length === 0 ) {
+        return 0
+    }
+    let sum = 0
+    for (let i = 0; i < array.length; i++) {
+        sum = sum + array[i]
+    }
+
+    return sum/array.length
 }
